01dce564dc072292b5cf2434c34cbfb769f79797
michael.wan <michael.wan@ichub.com>
2023-01-28 07:17:02 +0800

    20230128-笨办法学python习题7-2

--- 20230126/ex7_更多打印.py
+++ 20230126/ex7_更多打印.py
@@ -1,8 +1,13 @@
+# 打印句子
 print("Mary had a little lamb.")
+# 使用格式化字符打印句子
 print("Its fleece was white as {}.".format('snow'))
+# 打印句子
 print("And everywhere that Mary went.")
+# 打印10个英文句点
 print("." * 10)  # what'd that do?
 
+# 定义12个变量并赋值
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,7 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end.  try removing it to see what happens
+# 打印前面6个字符合并的字符串，结尾使用了一个空格
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# 打印了后面6个字符合并的字符串
 print(end7 + end8 + end9 + end10 + end11 + end12)
