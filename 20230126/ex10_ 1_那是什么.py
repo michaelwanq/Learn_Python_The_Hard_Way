ab4fa23c8c5599a837635cf1ea81560d7a7f11dd
michael.wan <michael.wan@ichub.com>
2023-01-28 17:47:54 +0800

    2023-01-28 17:47 commit

---
+++ 20230126/ex10_ 1_那是什么.py
@@ -0,0 +1,17 @@
+# 定义变量并赋值
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+#使用赋值中的各种格式进行打印输出
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
---
+++ 20230126/ex9_打印，打印，打印.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# 定义变量days，使用字符串进行赋值。
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# 定义变量months，使用字符串进行赋值。
+# 注意在字符串中包含换行符。
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# 使用"""*"""可以进行多行打印。
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
