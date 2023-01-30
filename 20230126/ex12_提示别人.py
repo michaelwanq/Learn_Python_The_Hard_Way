1786934e759d996c20f52e8914124a3eef2fce33
michael.wan <michael.wan@ichub.com>
2023-01-30 15:49:34 +0800

    2023-01-30 commit

---
+++ 20230126/ex12_提示别人.py
@@ -0,0 +1,6 @@
+# 将提示语放到input()函数中，先打印提示语字符串，获取的输入信息赋值给变量
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
